4c93489b5443b718b039398cacb61327a7301586
Trevor Grady <tgrady118@gmail.com>
2023-08-28 17:01:34 +0200

    woooo

--- Tests/DeepLinkHandlerTests/DeepLinkHandlerTests.swift
+++ Tests/DeepLinkHandlerTests/DeepLinkHandlerTests.swift
@@ -6,6 +6,5 @@ final class DeepLinkHandlerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DeepLinkHandler().text, "Hello, World!")
     }
 }
